757c06f4c07361416d0b9becbe657706f54034d9
shiva <qianwei4712@163.com>
2023-12-25 09:07:52 +0800

    文件前后统一是使用UTF-8

--- src/main/java/cn/shiva/controller/NovelController.java
+++ src/main/java/cn/shiva/controller/NovelController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
@@ -74,6 +75,7 @@ public class NovelController {
             return R.ok();
         }
         String content = aliOssComponent.streamDownload(novelFile.getOssPath());
+        content = new String(content.getBytes(StandardCharsets.UTF_8), StandardCharsets.UTF_8);
         return R.ok(FileDownloadBO.builder()
                 .content(content)
                 .name(novelFile.getName())
